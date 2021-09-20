b0e6dccde2b7e757160fd2418f61d194f30e77eb
Lucchomicucci <luchomicucci@gmail.com>
2021-09-20 19:39:29 -0300

    Agregado de resultados e imagenes

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
+let totalJugadas = 0;
+let ganadas = 0;
+let perdidas = 0;
+let empatadas = 0;
+
 const getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+const choice = (choice) => {
+  const choices = ['piedra', 'papel', 'tijera']
+  const choiceMachine = choices[getRandomInt(0, 3)]
+
+  if (choice == choiceMachine) {
+
+
+    empatadas += 1
+    document.getElementById("resultado").innerHTML =
+      `<h2>EMPATE la maquina eligio ${choiceMachine}.</h2>`
+
+  } else if (choice == 'piedra' && choiceMachine == 'tijera' ||
+    choice == 'papel' && choiceMachine == 'piedra' ||
+    choice == 'tijera' && choiceMachine == 'papel') {
+
+
+    ganadas += 1
+    document.getElementById("resultado").innerHTML = `<h2>GANASTE la maquina eligio ${choiceMachine}.</h2>`
+
+  } else {
+
+    perdidas += 1
+    document.getElementById("resultado").innerHTML = `<h2>PERDISTE la maquina eligio ${choiceMachine}.</h2>`
+
   }
-  
-  const choice = (choice) =>{
-    const choices = ['piedra', 'papel', 'tijera']
-    const choiceMachine = choices[getRandomInt(0, 3)]
-    
-    if(choice == choiceMachine){
-      console.log(`Elegiste ${choice}, la maquina eligio ${choiceMachine}. EMPATE `)
-    }else if(choice == 'piedra' && choiceMachine == 'tijera' ||
-             choice == 'papel' && choiceMachine == 'piedra' ||
-             choice == 'tijera' && choiceMachine == 'papel'){
-      console.log(`Elegiste ${choice}, la maquina eligio ${choiceMachine}. GANASTE `)
-    }else{
-      console.log(`Elegiste ${choice}, la maquina eligio ${choiceMachine}. PERDISTE `)
-    }
-  }
+
+  totalJugadas += 1
+  document.getElementById("historial").innerHTML = `<p>Total jugadas: ${totalJugadas}</p>
+      <p>Ganadas: ${ganadas}</p>
+      <p>Empatadas: ${empatadas}</p>
+      <p>Perdidas: ${perdidas}</p>`
+}
+const reset = () => {
+  totalJugadas = 0;
+  ganadas = 0;
+  perdidas = 0;
+  empatadas = 0;
+  document.getElementById("resultado").innerHTML = ``
+  document.getElementById("historial").innerHTML = `<p>Total jugadas: ${totalJugadas}</p>
+  <p>Ganadas: ${ganadas}</p>
+  <p>Empatadas: ${empatadas}</p>
+  <p>Perdidas: ${perdidas}</p>`
+}
